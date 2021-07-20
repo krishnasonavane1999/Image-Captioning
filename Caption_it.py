47e61da8dd71ce5be2cd2547f0aeac3895b50266
Krishna Sonavane <53361314+krishnasonavane1999@users.noreply.github.com>
2021-07-20 14:02:32 +0530

    Add files via upload

---
+++ Caption_it.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+# importing all necessary libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+import re
+import nltk
+import nltk.corpus as stopwords
+import string
+import json
+from time import time
+import pickle
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.preprocessing import image
+from keras.models import Model, load_model
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Input, Dense, Dropout, Embedding, LSTM
+from keras.layers.merge import add
+
+
+# In[34]:
+
+
+model = load_model("model_9.h5")
+
+
+# In[35]:
+
+
+model_temp = ResNet50(weights="imagenet", input_shape=(224, 224, 3))
+
+
+# In[38]:
+
+
+model_resnet = Model(model_temp.input, model_temp.layers[-2].output)
+
+
+# In[39]:
+
+
+def preprocess_img(img):
+    img = image.load_img(img, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis = 0)
+    # Normalisation
+    img = preprocess_input(img)
+    return img
+
+
+# In[48]:
+
+
+def encode_img(img):
+    img = preprocess_img(img)
+    feature_vector = model_resnet.predict(img)
+    feature_vector = feature_vector.reshape(1, feature_vector.shape[1])
+#     print(feature_vector.shape)
+    return feature_vector
+
+
+# In[73]:
+
+
+with open("word_to_idx.pkl", "rb") as w2i:
+    word_to_idx = pickle.load(w2i)
+    
+with open("idx_to_word.pkl", "rb") as i2w:
+    idx_to_word = pickle.load(i2w)
+
+
+# In[111]:
+
+
+def predict_caption(photo):
+    
+    in_text = "startseq"
+    max_len = 35
+    for i in range(max_len):
+        sequence = [word_to_idx[w] for w in in_text.split() if w in word_to_idx]
+        sequence = pad_sequences([sequence], maxlen = max_len, padding='post')
+        
+        ypred = model.predict([photo, sequence])
+        ypred = ypred.argmax() # word with max prob always - Greedy sampling
+        
+        word = idx_to_word[ypred]
+        in_text += (' ' + word)
+        
+        if word == "endseq":
+            break
+            
+        final_caption = in_text.split()
+        final_capion = final_caption[1:-1]
+        final_caption = ' '.join(final_caption)
+    final_caption = final_caption.split(' ', 1)[1]
+        
+    return final_caption
+
+
+# In[118]:
+
+def caption_this_image(image):
+
+    enc = encode_img(image)
+
+    ans = predict_caption(enc)
+
+    return ans
+
